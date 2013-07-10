93046a546e4cda22829e464c36abbad30ae31e3b
Wanchun Zhang <wanchunzhang@dyn208219.shef.ac.uk>
2013-07-10 13:08:08 +0100

    Initial Commit

---
+++ New@Shef/NSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NSAppDelegate.h
+//  New@Shef
+//
+//  Created by Wanchun Zhang on 10/07/2013.
+//  Copyright (c) 2013 Wanchun Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ New@Shef/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  New@Shef
+//
+//  Created by Wanchun Zhang on 10/07/2013.
+//  Copyright (c) 2013 Wanchun Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NSAppDelegate class]));
+    }
+}

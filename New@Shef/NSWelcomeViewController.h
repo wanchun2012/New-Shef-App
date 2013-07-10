80bcebcafd51aeb872ba440391aeee21c3fc7e08
Wanchun Zhang <wanchunzhang@dyn208219.shef.ac.uk>
2013-07-10 14:43:52 +0100

    *ios>iPhone>welcome page created

---
+++ New@Shef/NSWelcomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  NSWelcomeViewController.h
+//  New@Shef
+//
+//  Created by Wanchun Zhang on 10/07/2013.
+//  Copyright (c) 2013 Wanchun Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NSWelcomeViewController : UIViewController
+
+@end

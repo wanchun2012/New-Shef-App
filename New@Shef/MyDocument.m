99f40aa334c17e308a14617694f478f3dd3f6850
Wanchun Zhang <wanchunzhang@Wanchuns-MacBook-Pro.local>
2013-12-18 05:22:30 +0000

    *ios>iphone>icloud>add reference

--- New@Shef/MyDocument.m
+++ New@Shef/MyDocument.m
@@ -5,6 +5,8 @@
 //  Created by Wanchun Zhang on 01/09/2013.
 //  Copyright (c) 2013 Wanchun Zhang. All rights reserved.
 //
+//  Reference: 1. icloud and document
+//             http://www.techotopia.com/index.php/Using_iCloud_Storage_in_an_iOS_5_iPhone_Application
 
 #import "MyDocument.h"
 
